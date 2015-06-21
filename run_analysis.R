5e65f48f9472161f4ac6f3d83fc5a1298db9495a
SHILPA SWETH <shilpasweth@gmail.com>
2015-06-21 15:32:24 +0400

    run_analysis

--- run_analysis.R
+++ run_analysis.R
@@ -27,14 +27,14 @@ extract_features <- grepl("mean|std", features)  #Extracts the measurements on t
 X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")  #Loads the X_test data into X_test
 y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")  #Loads the y_test data into y_test
 subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")  #Loads the subject_test data into subject_test
-names(X_test) = features  #Loads data column names into X_test
+names(X_test) <- features  #Loads data column names into X_test
 
 
 
-X_test = X_test[,extract_features]  #Extracts the measurements on the mean and standard deviation for each measurement into X_test
-y_test[,2] = activity_labels[y_test[,1]]  #Loads activity labels into y_test
-names(y_test) = c("Activity_ID", "Activity_Label")  #Loads data column names into y_test
-names(subject_test) = "subject"  #Loads data column names into subject_test
+X_test <- X_test[,extract_features]  #Extracts the measurements on the mean and standard deviation for each measurement into X_test
+y_test[,2] <- activity_labels[y_test[,1]]  #Loads activity labels into y_test
+names(y_test) <- c("Activity_ID", "Activity_Label")  #Loads data column names into y_test
+names(subject_test) <- "subject"  #Loads data column names into subject_test
 
 test_data <- cbind(as.data.table(subject_test), y_test, X_test)  #Binds data of X_test and y_test together into test_data
 
@@ -43,25 +43,25 @@ test_data <- cbind(as.data.table(subject_test), y_test, X_test)  #Binds data of 
 X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")  #Loads the X_train data into X_train 
 y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")  #Loads the y_train data into y_train
 subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")  #Loads the subject_train data into subject_train
-names(X_train) = features  #Loads data column names into X_train
+names(X_train) <- features  #Loads data column names into X_train
 
 
 
-X_train = X_train[,extract_features]  #Extracts the measurements on the mean and standard deviation for each measurement into X_train
-y_train[,2] = activity_labels[y_train[,1]]  #Loads activity labels into y_train 
-names(y_train) = c("Activity_ID", "Activity_Label")  #Loads data column names into y_train
-names(subject_train) = "subject"  #Loads data column names into subject_train
+X_train <- X_train[,extract_features]  #Extracts the measurements on the mean and standard deviation for each measurement into X_train
+y_train[,2] <- activity_labels[y_train[,1]]  #Loads activity labels into y_train 
+names(y_train) <- c("Activity_ID", "Activity_Label")  #Loads data column names into y_train
+names(subject_train) <- "subject"  #Loads data column names into subject_train
 
 train_data <- cbind(as.data.table(subject_train), y_train, X_train)  #Binds data of X_train and y_train together into train_data
 
 
 
-data = rbind(test_data, train_data)  #Merges test and train data together into data
-id_labels   = c("subject", "Activity_ID", "Activity_Label")
-data_labels = setdiff(colnames(data), id_labels)
-melt_data      = melt(data, id = id_labels, measure.vars = data_labels)
+data <- rbind(test_data, train_data)  #Merges test and train data together into data
+id_labels <- c("subject", "Activity_ID", "Activity_Label")
+data_labels <- setdiff(colnames(data), id_labels)
+melt_data <- melt(data, id = id_labels, measure.vars = data_labels)
 
 
 
-tidy_data   = dcast(melt_data, subject + Activity_Label ~ variable, mean)  #Applies mean function to dataset
+tidy_data <- dcast(melt_data, subject + Activity_Label ~ variable, mean)  #Applies mean function to dataset
 write.table(tidy_data, file = "./tidy_data.txt",row.names=FALSE)  #Creates tidy data set called tidy_data
